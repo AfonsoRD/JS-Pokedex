728fd6f8ae559e8c3073564b92a89a0cc8a31ab9
Rui Afonso <afonso.r.dev@gmail.com>
2022-11-14 12:24:40 +0100

    Add Objects to pokemonList Array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
-let pokemonList = [];
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        type: ['grass', 'poison']
+    },
+    {
+        name: 'Charmander',
+        height: 0.6,
+        type: ['fire']
+    },
+    {
+        name: 'Squirtle',
+        height: 0.5,
+        type: ['water']
+    },
+    {
+        name: 'Pikachu',
+        height: 0.4,
+        type: ['electricity']
+    }
+
+];
