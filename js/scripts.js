a6461f21d20e22909dd315577c52e4888c101ebf
Rui Afonso <afonso.r.dev@gmail.com>
2022-11-15 12:39:44 +0100

    Change for loop to list pokemons and highlight a specific element
    Add some comments to JS file

--- js/scripts.js
+++ js/scripts.js
@@ -22,12 +22,15 @@ let pokemonList = [
 
 ];
 
-
-let text = " ";
-let i = 0
-for (;pokemonList[i];){
-  text = text + pokemonList[i].name + " (height: " + pokemonList[i].height +") ";
-  i++;
-}
-
-document.write(text);
+//for loop that iterates over each item in pokemonList and will highlight one pokemon with height superior than 0.6
+/* add tag <br> for break and <strong> for bold text:
+this inline css will be removed when start to structure the html
+*/
+for (let i = 0; i < pokemonList.length; i++) {
+    
+        if(pokemonList[i].height > 0.6) {
+            document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') -' + ' <strong>Wow, that\’s big!</strong><br>');
+        } else {
+            document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')<br>');
+        }
+    }
