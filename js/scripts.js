c1bd4a169f1e88d11612918edab95ac287766cc5
Rui Afonso <afonso.r.dev@gmail.com>
2022-11-24 14:10:24 +0100

    Add functionality to close the modal

--- js/scripts.js
+++ js/scripts.js
@@ -71,35 +71,79 @@ let pokemonRepository = (function () {
     let modal = document.createElement("div");
     modal.classList.add("modal");
 
-    /* create close and add fav button*/
+    /* create close button*/
     let closeButtonElement = document.createElement("button");
-    let favButtonElement = document.createElement("button");
     closeButtonElement.classList.add(".modal-close");
     closeButtonElement.innerHTML = "Close";
+    closeButtonElement.addEventListener("click", hideModal);
+
+    /* create fav button*/
+    let favButtonElement = document.createElement("button");
     favButtonElement.classList.add(".modal-fav");
     favButtonElement.innerHTML = "❤️";
+    favButtonElement.addEventListener("click", addFavourite);
 
     /*Title with Pokemon name*/
     let titleElement = document.createElement("h1");
     titleElement.innerHTML = title;
 
     /*Add img*/
+    let imageContainer = document.createElement("div");
+    imageContainer.classList.add(".pokemon-img");
     let imageElement = document.createElement("img");
     imageElement.src = " ";
+    imageElement.alt = "Pokemon image";
 
-    /* Element to display text*/
-    let contentElement = document.createElement("p");
-    titleElement.innerHTML = text;
+    /* Element to display weight and height*/
+    let contentElement = document.createElement("div");
+    contentElement.classList.add("content-container");
+    let measureElement = document.createElement("p");
+    measureElement.innerHTML = "Weight: X and Height: Y";
+
+    /* Element to display Type*/
+    let contentTypeElement = document.createElement("div");
+    contentTypeElement.classList.add(".type-container");
+    let typeElement = document.createElement("p");
+    typeElement.innerHTML = "Type: X";
 
     modal.appendChild(closeButtonElement);
     modal.appendChild(favButtonElement);
     modal.appendChild(titleElement);
     modal.appendChild(imageElement);
     modal.appendChild(contentElement);
+    modal.appendChild(measureElement);
+    modal.appendChild(contentTypeElement);
+    modal.appendChild(typeElement);
     modalContainer.appendChild(modal);
 
     modalContainer.classList.add("is-visible");
   }
+
+  /* hide the Modal*/
+  function hideModal() {
+    let modalContainer = document.querySelector("#modal-container");
+    modalContainer.classList.remove("is-visible");
+  }
+
+  /* Add pokemon to Fav list */
+
+  function addFavourite() {}
+
+  /* Keydown event listener, 'When press ESC close the modal'*/
+  window.addEventListener("keydown", (e) => {
+    let modalContainer = document.querySelector("#modal-container");
+    if (e.key === "Escape" && modalContainer.classList.contains("is-visible")) {
+      hideModal();
+    }
+  });
+
+  let modalContainer = document.querySelector("#modal-container");
+  modalContainer.addEventListener("click", (e) => {
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
 
   function loadList() {
     showLoadingMessage();
