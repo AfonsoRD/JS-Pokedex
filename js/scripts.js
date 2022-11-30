a94846e738a478d6e18f8ecf32e4aca0dd07545f
Rui Afonso <afonso.r.dev@gmail.com>
2022-11-30 19:43:47 +0100

    convert modal with bootstrap components, display List as grid

--- js/scripts.js
+++ js/scripts.js
@@ -35,17 +35,21 @@ let pokemonRepository = (function () {
   //forEach loop for pokemon list
   function addListItem(pokemon) {
     let pokemonList = document.querySelector(".pokemon-list");
-
     let listPokemon = document.createElement("li");
     listPokemon.classList.add("list-group-item");
 
     let button = document.createElement("button");
     button.innerText = pokemon.name;
-
-    eventListener(button, pokemon);
     button.classList.add("button");
+    button.setAttribute("data-toggle", "modal");
+    button.setAttribute("data-target", "#pokemon-modal");
+    button.classList.add("col");
+
+    listPokemon.classList.add("list-items");
     listPokemon.appendChild(button);
     pokemonList.appendChild(listPokemon);
+
+    eventListener(button, pokemon);
   }
 
   function eventListener(button, pokemon) {
@@ -54,86 +58,39 @@ let pokemonRepository = (function () {
     });
   }
 
-  let modalContainer = document.querySelector("#modal-container");
-
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function () {
-      /* Implement Modal*/
-      /*Select container from HTML page*/
-      modalContainer.innerText = " ";
-
-      /*Create DIV and add modal on DOM*/
-      let modal = document.createElement("div");
-      modal.classList.add("modal");
-
-      /* create close button */
-      let closeButtonElement = document.createElement("button");
-      closeButtonElement.classList.add("modal-close");
-      closeButtonElement.innerHTML = "Close";
-      closeButtonElement.addEventListener("click", hideModal);
-
-      /* create fav button */
-      let favButtonElement = document.createElement("button");
-      favButtonElement.classList.add("modal-fav");
-      favButtonElement.innerHTML = "❤️";
-      favButtonElement.addEventListener("click", addFavourite);
-
-      /*Title with Pokemon name */
-      let pokemonName = document.createElement("h1");
-      pokemonName.classList.add("modal-title");
-      pokemonName.innerHTML = `#${pokemon.id} ${pokemon.name}`;
-
-      /*Add img */
-
-      let pokemonImage = document.createElement("img");
-      pokemonImage.classList.add("pokemon-img");
-      pokemonImage.src = pokemon.imageUrl;
-      pokemonImage.alt = "Pokemon image";
-
-      /* Element to display weight and height */
-      let contentElement = document.createElement("p");
-      contentElement.classList.add("content-container");
-      contentElement.innerHTML = `Weight: ${pokemon.weight} / Height: ${pokemon.height}`;
-
-      /* Element to display Type */
-      let contentTypeElement = document.createElement("p");
-      contentTypeElement.classList.add("type-container");
-      contentTypeElement.innerHTML = `Type: ${pokemon.types}`;
-
-      modal.appendChild(closeButtonElement);
-      modal.appendChild(favButtonElement);
-      modal.appendChild(pokemonName);
-      modal.appendChild(pokemonImage);
-      modal.appendChild(contentElement);
-      modal.appendChild(contentTypeElement);
-      modalContainer.appendChild(modal);
-
-      modalContainer.classList.add("is-visible");
+      showModal(pokemon);
     });
   }
 
-  /* hide the Modal*/
-  function hideModal() {
-    modalContainer.classList.remove("is-visible");
+  function showModal(pokemon) {
+    let modalBody = $(".modal-body");
+    let modalTitle = $(".modal-title");
+
+    modalTitle.empty();
+    modalBody.empty();
+
+    //creating element for name
+
+    let nameElement = $("<h1>" + pokemon.id + "# " + pokemon.name + "</h1>");
+    //creating element for img
+    let imageElement = $('<img class="modal-img" style="width:50%">');
+    imageElement.attr("src", pokemon.imageUrl);
+
+    //creating element for height and weight
+    let heightElement = $("<p>" + "Height: " + pokemon.height + "</p>");
+    let weightElement = $("<p>" + "Weight: " + pokemon.weight + "</p>");
+
+    //creating element for types
+    let typesElement = $("<p>" + "Type: " + pokemon.types + "</p>");
+
+    modalTitle.append(nameElement);
+    modalBody.append(imageElement);
+    modalBody.append(heightElement);
+    modalBody.append(weightElement);
+    modalBody.append(typesElement);
   }
-
-  /* Add pokemon to Fav list */
-
-  function addFavourite() {}
-
-  /* Keydown event listener, 'When press ESC close the modal'*/
-  window.addEventListener("keydown", (e) => {
-    if (e.key === "Escape" && modalContainer.classList.contains("is-visible")) {
-      hideModal();
-    }
-  });
-
-  modalContainer.addEventListener("click", (e) => {
-    let target = e.target;
-    if (target === modalContainer) {
-      hideModal();
-    }
-  });
 
   function loadList() {
     showLoadingMessage();
@@ -183,9 +140,10 @@ let pokemonRepository = (function () {
     getAll: getAll,
     add: add,
     addListItem: addListItem,
+    showDetails: showDetails,
     loadList: loadList,
     loadDetails: loadDetails,
-    showDetails: showDetails
+    showModal: showModal
   };
 })();
 
